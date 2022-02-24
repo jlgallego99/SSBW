2ccc9c468e20715d5967fe3b1a11d406ac898159
Dunspa <jlgallego99@correo.ugr.es>
2022-02-24 17:59:28 +0100

    Modelo de usuarios

--- populate.py
+++ populate.py
@@ -1,4 +1,13 @@
-from mongoengine import *
+from mongoengine import connect, Document, EmbeddedDocument
+from mongoengine.fields import EmbeddedDocumentField, StringField, ListField, IntField, DateTimeField
+from datetime import datetime
 from decouple import config
 
 connect(host=config("MONGOURI"))
+
+class Users(Document):
+    first_name = StringField(max_length=50)
+    last_name = StringField(max_length=50)
+    email = StringField(required=True)
+    phone = StringField(max_length=15)
+    gender = StringField(max_length=6)
