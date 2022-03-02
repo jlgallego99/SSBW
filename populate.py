4c4f16d9ce9ddb8c376d80cba14836de04d4ac46
Dunspa <jlgallego99@correo.ugr.es>
2022-03-02 19:48:28 +0100

    Añadir usuarios prueba

--- populate.py
+++ populate.py
@@ -3,7 +3,7 @@ from mongoengine.fields import EmbeddedDocumentField, StringField, ListField, In
 from datetime import datetime
 from decouple import config
 
-connect(host=config("MONGOURI"))
+connect('ssbw', host='mongo')
 
 class Users(Document):
     first_name = StringField(max_length=50)
@@ -11,3 +11,17 @@ class Users(Document):
     email = StringField(required=True)
     phone = StringField(max_length=15)
     gender = StringField(max_length=6)
+
+# Add ten users
+users = []
+for x in range(0, 11):
+    user = Users()
+    user.first_name = "hola"
+    user.last_name = "hola"
+    user.email = "hola"
+    user.phone = "hola"
+    user.gender = "hola"
+
+    users.append(user)
+
+Users.objects.insert(users)
