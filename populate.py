a00b8ebb26c2de8f80e45a017c70b8a2a53b0c44
Dunspa <jlgallego99@correo.ugr.es>
2022-03-02 20:14:15 +0100

    Consumir fakerapi
    para añadir usuarios ficticios

--- populate.py
+++ populate.py
@@ -1,3 +1,4 @@
+import requests
 from mongoengine import connect, Document, EmbeddedDocument
 from mongoengine.fields import EmbeddedDocumentField, StringField, ListField, IntField, DateTimeField
 from datetime import datetime
@@ -12,16 +13,16 @@ class Users(Document):
     phone = StringField(max_length=15)
     gender = StringField(max_length=6)
 
-# Add ten users
+# Add ten fake users
 users = []
-for x in range(0, 11):
-    user = Users()
-    user.first_name = "hola"
-    user.last_name = "hola"
-    user.email = "hola"
-    user.phone = "hola"
-    user.gender = "hola"
-
-    users.append(user)
+r = requests.get("https://fakerapi.it/api/v1/persons?_quantity=10")
+for user in r.json()["data"]:
+    u = Users()
+    u.first_name = user["firstname"]
+    u.last_name = user["lastname"]
+    u.email = user["email"]
+    u.phone = user["phone"]
+    u.gender = user["gender"]
+    users.append(u)
 
 Users.objects.insert(users)
