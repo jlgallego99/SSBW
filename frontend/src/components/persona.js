02cff06c8504fd45f98689957bfd1a4305d05fdd
Dunspa <jlgallego99@correo.ugr.es>
2022-06-18 21:39:05 +0200

    Fetch de persona

--- frontend/src/components/persona.js
+++ frontend/src/components/persona.js
@@ -2,15 +2,39 @@ import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import { useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 export default function Persona() {
     const { id } = useParams();
+    const [isLoading, setIsLoading] = useState(true);
+    const [person, setPerson] = useState(null);
+    const navigate = useNavigate();
 
-    return (
-        <Box>
-            <Typography variant="body1">
-                {id}
-            </Typography>
-        </Box>
-    )
+    const fetchPersonList = () => {
+        setIsLoading(true);
+        fetch("http://localhost:80/app/api/person/" + id)
+            .then((response) => response.json())
+            .then((p) => {
+                setPerson(p);
+                setIsLoading(false);
+            });
+    };
+    useEffect(fetchPersonList, []);
+
+    if (isLoading) {
+        return (
+            <>
+                <h1>Cargando...</h1>
+            </>
+        )
+    } else {
+        return (
+            <Box>
+                <Typography variant="body1">
+                    {person.firstName} {person.lastName}
+                </Typography>
+            </Box>
+        )
+    }
 }
